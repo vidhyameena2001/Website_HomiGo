d3d8be0da2080dc138ef4b79d533057e77d1f592
vidhyameena2001 <vidhyameena2001@gmail.com>
2025-08-11 22:35:23 +0530

    Initial commit

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  state = { hasError: false, error: null };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <p>{this.state.error?.message}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
